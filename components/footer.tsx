066b37df27368922781d13df3943a84f404d6308
mikeunge <mikeunge@protonmail.com>
2025-04-13 18:51:51 +0200

    fix: pass text color as prop

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,13 +1,13 @@
 import Link from 'next/link';
 
-export default function Footer() {
+export default function Footer({ textColor = 'text-gray-800' }) {
   return (
     <div className="flex flex-col items-center">
-      <p className="text-gray-800 text-center mt-10">
+      <p className={`${textColor} text-center mt-10`}>
         <Link href="/faq">FAQ</Link> - <Link href="/privacy-policy">Privacy Policy</Link> -{' '}
         <Link href="/terms-of-service">Terms of Service</Link>
       </p>
-      <p className="text-gray-800 text-center mt-1">
+      <p className={`${textColor} text-center mt-1`}>
         <b>Shareify</b> is a <b>free</b> and <b>open-source</b> project. If you like it, please
         consider supporting me on{' '}
         <b>
