d0065902e8fec8b740d3c2e3af962f7ec1dc11d9
mikeunge <mikeunge@protonmail.com>
2025-04-13 22:34:27 +0200

    fix: add new spotify cdn

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL('https://mosaic.scdn.co/**')]
+    remotePatterns: [
+      new URL('https://mosaic.scdn.co/**'),
+      new URL('https://image-cdn-ak.spotifycdn.com/**')
+    ]
   }
 };
 
