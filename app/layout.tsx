174372cdd2f4ab2edbd2036b50982e36aff7ed84
mikeunge <mikeunge@protonmail.com>
2025-04-14 19:38:47 +0200

    chore: add meta tags to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import type React from 'react';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
@@ -24,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
