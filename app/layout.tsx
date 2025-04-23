520f3bb482c5b2c692465f69468acf4395a7b196
mikeunge <mikeunge@protonmail.com>
2025-04-23 19:08:43 +0200

    fix: move <Analytics /> into layout body

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,10 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
-      <Analytics />
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
